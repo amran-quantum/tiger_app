11db866db945e021b06acff6c8555c1f9e0681ad
amran-quantum <amran@quantumuniversal.com>
2021-09-30 19:12:43 +0600

    60% leave statistics

--- tiger_app/tiger_app/report/leave_statistics/leave_statistics.py
+++ tiger_app/tiger_app/report/leave_statistics/leave_statistics.py
@@ -7,43 +7,54 @@ def execute(filters=None):
 	columns, data = [], []
 
 	columns = get_columns()
-	data = get_data(filters,columns)
+	data = get_data(filters)
 	return columns, data
 def get_columns():
 	leave_type_list = frappe.db.sql("""
 	select distinct leave_type from `tabLeave Allocation` order by leave_type asc
 	""",as_dict=True)
-	cols = ["Employee Name"]
+	cols = ["Employee","Employee Name"]
 	for el in leave_type_list:
 		cols.append(el.leave_type+" Start")
 		cols.append(el.leave_type+" End")
 		cols.append("Total")
 	return cols
+	# return []
 
 	
 
-def get_data(filters,columns):
+def get_data(filters):
 	data = frappe.db.sql("""
-	select * from `tabLeave Application` where from_date >= %(from_date)s  and to_date <= %(to_date)s and status=%(status)s
-	order by leave_type asc""",{"from_date":filters.from_date,"to_date":filters.to_date,"status":"Approved"},as_dict=1)
+	select employee, employee_name, leave_type, from_date, to_date, total_leave_days from `tabLeave Application` where from_date >= %(from_date)s  and to_date <= %(to_date)s and status=%(status)s
+	""",{"from_date":filters.from_date,"to_date":filters.to_date,"status":"Approved"},as_dict=1)
+	
+	leave_type_list = frappe.db.sql("""select distinct leave_type from `tabLeave Allocation`""",as_dict=True)
+	employee_list = frappe.db.sql("""select distinct employee, employee_name from `tabLeave Allocation`""",as_dict=True)
+	
 	result = []
+	for em in employee_list:
+		edata = []
+		for elem in data:
+			if elem.employee == em.employee:
+				edata.append(elem)
+		sorted_row = []
+		for el in edata:
+			if not any(el.employee in d for d in result):
+				sorted_row.append(el.employee)
+				sorted_row.append(el.employee_name)
+			else:
+				sorted_row.append("")
+				sorted_row.append("")
 
-	leave_type_list = frappe.db.sql("""select distinct leave_type from `tabLeave Allocation` order by leave_type asc""",as_list=True)
-	for elem in data:
-		row = []
-		row.append(elem.employee_name)
-
-		for el in leave_type_list:
-			if el == elem.leave_type:
-				row.append(elem.from_date)
-				row.append(elem.to_date)
-				row.append(elem.total_leave_days)
-			
-			
-		
-		result.append(row)
-
-	
+			for item in leave_type_list:
+				if item.leave_type == el.leave_type:
+					sorted_row.append(el.from_date)
+					sorted_row.append(el.to_date)
+					sorted_row.append(el.total_leave_days)
+				else:
+					sorted_row.append("")
+					sorted_row.append("")
+					sorted_row.append("")
+			result.append(sorted_row)
+			# frappe.msgprint(str(sorted_row))
 	return result
-	
-		
