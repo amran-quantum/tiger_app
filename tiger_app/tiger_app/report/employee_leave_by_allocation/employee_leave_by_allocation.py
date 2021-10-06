cc56a41bd33f0eab6267207bc12d68601e525289
amran-quantum <amran@quantumuniversal.com>
2021-10-06 14:30:22 +0600

    emp leave by allocation fixed with unpaid

--- tiger_app/tiger_app/report/employee_leave_by_allocation/employee_leave_by_allocation.py
+++ tiger_app/tiger_app/report/employee_leave_by_allocation/employee_leave_by_allocation.py
@@ -10,6 +10,7 @@ def execute(filters=None):
 	columns, data = [], []
 
 	columns, leave_types = get_columns()
+	columns.append("Unpaid Leave")
 	if filters.year:
 		filters.update({
 			'from_date': filters.year+"-01-01",
@@ -34,31 +35,39 @@ def execute(filters=None):
 	for item in fdata:
 		row = [item.department_code,item.employee_name]
 
+		for elem in leave_types:
+			if "Annual" in elem:
+				lal_details = frappe.get_list('Leave Allocation', filters={'employee':item.employee,'leave_type':['like', '%Annual%'] }, fields={'leave_type','total_leaves_allocated','carry_forwarded_leaves_count'})
+				cf = lal_details[0].carry_forwarded_leaves_count
+
+		acm_date = filters.acm_date.split('-')
+		a = date(int(acm_date[0]),int(acm_date[1]),int(acm_date[2]))
+		today = date.today()
+		b = date(int(today.strftime("%Y")),1,1)
+		acm = (a-b).days
+		acm = (lal_details[0].total_leaves_allocated / 365)*acm + (lal_details[0].carry_forwarded_leaves_count or 0)
+		acm = round(acm)
+		row.append(acm)
+		row.append(cf)
+
 		for e in leave_types:
 			ldata = ""
 			if "Annual" in e:
-				leave_details = frappe.get_list('Leave Allocation', filters={'employee_name':item.employee_name,'leave_type':['like', '%Annual%'] }, fields={'leave_type','total_leaves_allocated'})
+				leave_details = frappe.get_list('Leave Allocation', filters={'employee':item.employee,'leave_type':['like', '%Annual%'] }, fields={'leave_type','total_leaves_allocated'})
 				ldata = leave_details[0].leave_type
 			else:
-				leave_details = frappe.db.sql("""select * from `tabLeave Allocation` where leave_type=%(ltn)s and employee_name=%(emp_name)s""",{"ltn":e,"emp_name":item.employee_name},as_dict=1)
+				leave_details = frappe.db.sql("""select * from `tabLeave Allocation` where leave_type=%(ltn)s and employee=%(employee)s""",{"ltn":e,"employee":item.employee},as_dict=1)
 				ldata = e
 			
 			leave_application = frappe.get_list('Leave Application',
-			filters={"employee_name": item.employee_name,
+			filters={"employee": item.employee,
 						"status": "Approved",
 						"leave_type": ldata,
 						"from_date": ['>=', filters.from_date],
 						"to_date": ['<=', filters.to_date]},
 			fields={"total_leave_days"}
 						)
-			# acm_date = filters.acm_date.split('-')
-			# a = date(int(acm_date[0]),int(acm_date[1]),int(acm_date[2]))
-			# today = date.today()
-			# b = date(int(today.strftime("%Y")),1,1)
-			# acm = (a-b).days
-			# acm = (leave_details[0].total_leaves_allocated / 365)*acm + (leave_details[0].carry_forwarded_leaves_count or 0)
-			# acm = round(acm)
-			# row.append(acm)
+			
 			if len(leave_application)> 0: 			
 				total = leave_details[0].total_leaves_allocated
 				cf = leave_details[0].carry_forwarded_leaves_count
@@ -68,30 +77,42 @@ def execute(filters=None):
 					taken += elem.total_leave_days
 				remaining = (cf or 0)+ total - taken
 
-				# row.append(cf or 0)
 				row.append(total)
 				row.append(taken)
 				row.append(remaining)
 			else:
-
-
 				if "Annual" in e:
-					leave_allocation = frappe.db.count('Leave Allocation', {'employee_name':item.employee_name,'leave_type':['like', '%Annual%'] })
+					leave_allocation = frappe.db.count('Leave Allocation', {'employee':item.employee,'leave_type':['like', '%Annual%'] })
 				else:
-					leave_allocation = frappe.db.count('Leave Allocation', {'employee_name':item.employee_name,'leave_type': e })
+					leave_allocation = frappe.db.count('Leave Allocation', {'employee':item.employee,'leave_type': e })
 				if leave_allocation > 0:
-					# row.append(leave_details[0].carry_forwarded_leaves_count or 0)
 					row.append(leave_details[0].total_leaves_allocated)
 					row.append(0)
 					row.append((leave_details[0].carry_forwarded_leaves_count or 0)+leave_details[0].total_leaves_allocated)
 				else:
-					# row.append(0)
 					row.append(0)
 					row.append(0)
 					row.append(0)
-				
-					
+		
+		# count leave without pay
+		# check and find all leave without pay as list 
+		lwp = frappe.get_list('Leave Type',filters={'is_lwp':1},fields={'leave_type_name'},pluck='leave_type_name')		
+		
+		# calculate sum of unpaid leaves taken by employee
+		ul = frappe.db.sql("""
+		select sum(total_leave_days) from `tabLeave Application` where
+		employee=%(employee)s and
+		leave_type in %(leave_types_unpaid)s and
+		status=%(status)s
+		""",{"employee":item.employee,"leave_types_unpaid":tuple(lwp),"status":"Approved"},as_dict=True)
+
+		# push total unpaid leave in line
+		row.append(ul[0]["sum(total_leave_days)"] or 0)
+		
+		# push full line
 		data.append(row)
+
+
 	return columns, data
 
 
@@ -99,8 +120,8 @@ def get_columns():
 	columns = [
 		 _("Department Code") + "::140",
 		 _("Employee Name") + "::140",
-		#  _("Accumulation") + "::140",
-		#  _("Annual Leave Carry Forward") + "::140"
+		 _("Accumulation") + "::140",
+		 _("Ballance from last year") + "::140"
 	]
 	leave_types = {_("lt_list"): []}
 	leave_allocation = frappe.db.sql("""select distinct leave_type from `tabLeave Allocation`""",as_dict=1)
@@ -120,7 +141,7 @@ def get_columns():
 def get_data(filters):
 	data = frappe.db.sql(
 		"""
-		select distinct ta.employee_name, em.department_code from `tabLeave Allocation` ta, `tabEmployee` em where from_date>=%(from_date)s and to_date<=%(to_date)s and ta.employee_name=em.employee_name
+		select distinct ta.employee_name, ta.employee, em.department_code from `tabLeave Allocation` ta, `tabEmployee` em where from_date>=%(from_date)s and to_date<=%(to_date)s and ta.employee_name=em.employee_name
 		"""
 	,{"from_date":filters.from_date, "to_date":filters.to_date},as_dict=1)
 	
